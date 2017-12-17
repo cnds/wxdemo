3640e67b68ecbf3b0dbb9f43e9e0f58108696724
丁松 <dingsong87@gmail.com>
2017-12-17 23:45:09 +0800

    add wx crypt util

--- wxbase/utils/__init__.py
+++ wxbase/utils/__init__.py
@@ -3,3 +3,4 @@ from .jwt_base import JWTBase
 from .util_base import UtilBase
 from .request_endpoint import RequestEndpoint
 from .verification_code_generator import generate_verification_code
+from .wx_biz_data_crypt import WXBizDataCrypt
