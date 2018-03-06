e17c93714b5ff32922950e37ccbe18a2f6c3ffd1
丁松 <dingsong87@gmail.com>
2018-03-06 11:47:45 +0800

    add bind collect code page

--- ministore/pages/index/index.js
+++ ministore/pages/index/index.js
@@ -1,4 +1,5 @@
 // pages/index/index.js
+var status = require('../../utils/error.js')
 const app = getApp()
 
 Page({
@@ -20,18 +21,75 @@ Page({
   },
 
   bindQRCode: function (e) {
+    var that = this
     wx.scanCode({
       scanType: ['qrCode'],
       success: function (res) {
+        console.log(res)
         // var scene = decodeURIComponent(res.scene)
-        var scene = 'T001'
+        var scene = 'testqrcode'
         // 传入商铺的id和支付信息
         wx.request({
           url: 'http://localhost:10000/gateway/stores/' + app.globalData.storeInfo.id + '/bind-qr-code',
           method: 'POST',
-          data: { scene: scene, storeId: app.globalData.storeInfo.id },
+          header: { 'Authorization': 'Bearer ' + app.globalData.storeInfo.token },
+          data: { QRCode: scene },
           success: function (res) {
-            console.log(res)
+            if (res.statusCode === 201) {
+              wx.showActionSheet({
+                itemList: ['继续绑定收款码', '下次再说'],
+                success: function (res) {
+                  if (res.tapIndex === 0) {
+                    that.bindCollectCode()
+                  } else if (res.tapIndex === 1) {
+                    console.log(res)
+                  }
+                }
+              })
+            } else if (res.statusCode === 400) {
+              if (res.data.error === 'QR_CODE_ALREADY_BEEN_BOUND') {
+                wx.showActionSheet({
+                  itemList: ['继续绑定收款码', '下次再说'],
+                  success: function(res) {
+                    if (res.tapIndex === 0) {
+                      that.bindCollectCode()
+                    } else if (res.tapIndex === 1) {
+                      console.log(res)
+                    }
+                  }
+                })
+              } else {
+              status.status400(res.data.error)
+              }
+            } else {
+              status.status500()
+            }
+          }
+        })
+      }
+    })
+  },
+
+  bindCollectCode: function(e) {
+    wx.scanCode({
+      scanType: ['qrCode'],
+      success: function (res) {
+        console.log(res)
+        var wechatInfo = res.result
+        wx.request({
+          url: 'http://localhost:10000/gateway/stores/' + app.globalData.storeInfo.id + '/bind-collect-code',
+          method: 'POST',
+          data: { WechatInfo: wechatInfo },
+          success: function (res) {
+            if (res.statusCode === 201) {
+              wx.showToast({
+                title: '绑定收款码成功'
+              })
+            } else if (res.statusCode === 400) {
+              status.status400(res.data.error)
+            } else {
+              status.status500()
+            }
           }
         })
       }
