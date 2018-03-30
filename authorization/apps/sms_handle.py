c429060ebfc0dff4bd2c5f64b6a04579c707d1f3
丁松 <dingsong87@gmail.com>
2018-03-30 10:48:41 +0800

    modify sms post api ref aliyun sdk

--- authorization/apps/sms_handle.py
+++ authorization/apps/sms_handle.py
@@ -1,26 +1,55 @@
+import uuid
+import json
 from flask import request, jsonify
-from apps.base import BaseHandler
-from apps.json_validate import SCHEMA
+from .base import BaseHandler
+from .json_validate import SCHEMA
 from jybase.utils import generate_verification_code
+from aliyunsdkdysmsapi.request.v20170525 import SendSmsRequest
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.profile import region_provider
+from config import config
 
 
 class SmsHandler(BaseHandler):
 
-    sms_info = {
-        'store_reset_password': '您正在申请重置商户密码, 验证码是%s, 有效期5分钟。',
-        'store_sign_up': '您正在注册商户账号, 验证码是%s, 有效期5分钟。',
-    }
+    REGION = "cn-hangzhou"
+    PRODUCT_NAME = "Dysmsapi"
+    DOMAIN = "dysmsapi.aliyuncs.com"
 
-    def send_sms(self, mobile, verify_type):
-        code = generate_verification_code(include_letter=False,
-                                          include_upper=False)
-        text = self.sms_info.get(verify_type) % code
-        print(text)
-        redis_key = verify_type + ':' + mobile + ':'
-        self.redis.set_value(redis_key, code)
-        # TODO: send sms via sms service
-        # send_sms_function(text)
-        return True
+    acs_client = AcsClient(config['sms']['access_key_id'],
+                           config['sms']['access_key_secret'], REGION)
+    region_provider.add_endpoint(PRODUCT_NAME, REGION, DOMAIN)
+
+    # from aliyun demo
+    def send_sms(self, business_id, phone_numbers, sign_name, template_code,
+                 template_param=None):
+        smsRequest = SendSmsRequest.SendSmsRequest()
+        # 申请的短信模板编码,必填
+        smsRequest.set_TemplateCode(template_code)
+
+        # 短信模板变量参数
+        if template_param is not None:
+            smsRequest.set_TemplateParam(template_param)
+
+        # 设置业务请求流水号，必填。
+        smsRequest.set_OutId(business_id)
+
+        # 短信签名
+        smsRequest.set_SignName(sign_name)
+
+        # 数据提交方式
+        # smsRequest.set_method(MT.POST)
+
+        # 数据提交格式
+        # smsRequest.set_accept_format(FT.JSON)
+
+        # 短信发送的号码列表，必填。
+        smsRequest.set_PhoneNumbers(phone_numbers)
+
+        # 调用短信发送接口，返回json
+        smsResponse = self.acs_client.do_action_with_exception(smsRequest)
+
+        return json.loads(smsResponse)
 
     def post(self):
         is_valid, data = self.get_params_from_request(request,
@@ -30,6 +59,14 @@ class SmsHandler(BaseHandler):
 
         verify_type = data['verifyType']
         mobile = data['mobile']
-        result = self.send_sms(mobile, verify_type)
-        if result:
-            return jsonify({'id': verify_type}), 201
+        code = generate_verification_code(include_letter=False,
+                                          include_upper=False)
+        business_id = uuid.uuid1()
+        params = json.dumps({'code': code})
+        result = self.send_sms(business_id, mobile, config['sms']['sign'],
+                               config['sms']['template'][verify_type], params)
+        result_code = result['Code']
+        if result_code == 'OK':
+            redis_key = verify_type + ':' + mobile + ':'
+            self.redis.set_value(redis_key, code)
+            return jsonify({'id': result['RequestId']}), 201
